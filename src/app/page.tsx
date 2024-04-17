d437cc39c1805c446db3882cbf4cde52ebb76516
ImSreyas <sreyassatheesh755@gmail.com>
2024-04-17 15:59:55 +0530

    Folderls populated.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,22 @@
+"use client"
+
+import { PrismaClient } from '@prisma/client'
 import React from 'react'
 
+const prisma = new PrismaClient();
+
 const Home = () => {
+  const handleClick  = async () => {
+    const data = await prisma.User.create({
+      email: "sreyas@gmail.com"
+    })
+    console.log(data);
+  }
+
   return (
-    <div>page</div>
+    <div>
+      <button onClick={handleClick}>hello</button>
+    </div>
   )
 }
 
