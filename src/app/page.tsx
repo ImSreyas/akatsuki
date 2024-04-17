f83c897f039ca0627f1326aefc7d4c72ed693d52
ImSreyas <sreyassatheesh755@gmail.com>
2024-04-17 18:06:25 +0530

    Prisma issue solved.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,22 @@
 import { UserProfile, auth ,SignOutButton} from '@clerk/nextjs'
+import { PrismaClient } from '@prisma/client';
 import React from 'react'
+
+const prisma = new PrismaClient();
 
 const Home = async () => {
   const user = await auth();
+
+  const putUser = async () => {
+    prisma.$connect();
+    const data = await prisma.user.create({
+      data: {
+        email: "manu@gmail.com",
+        name: "manu"
+      }
+    })
+  }
+  await putUser();
 
   return (
     <div>
