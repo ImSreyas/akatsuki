03c95404a3bc84c6cbfa4083dba76eb624a30f8e
Aswin K O <aswinko479@gmail.com>
2024-04-18 00:03:27 +0530

    component navigation

--- src/app/donation/page.tsx
+++ src/app/donation/page.tsx
@@ -1,14 +1,24 @@
-import { UserProfile, auth ,SignOutButton} from '@clerk/nextjs'
-import React from 'react'
+import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger } from "@/components/ui/navigation-menu";
+import { UserProfile, auth, SignOutButton } from "@clerk/nextjs";
+import React from "react";
 
 const Donation = async () => {
   const user = await auth();
 
   return (
     <div>
-      {/* <SignIn /> */}
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <NavigationMenuLink>Link</NavigationMenuLink>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>{" "}
     </div>
-  )
-}
+  );
+};
 
-export default Donation
+export default Donation;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,21 @@ import { UserProfile, auth ,SignOutButton} from '@clerk/nextjs'
 import React from 'react'
 
 const Home = async () => {
-  const user = await auth();
+  // const user = await auth();
 
-  const data = await  prisma.user.create({
-   data: {
-    email: "aswinko@gmail.com",
-     name: "Aswin",
-   }
+  try {
+    const newUser = await prisma.users.create({
+      data: {
+        email: "aswinko@gmail.com",
+        name: "Aswin",
+        // Add any other required fields here
+      }
+    });
+    console.log("User created successfully:", newUser);
+  } catch (error) {
+    console.error("Error creating user:", error);
+  }
 
-  
-
-  })
 
   return (
     <div>
