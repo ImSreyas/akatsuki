ac9bd6e3c92a1caa4c4e5a0462063dbdee7f341b
ImSreyas <sreyassatheesh755@gmail.com>
2024-04-18 07:38:26 +0530

    Minor chnages.

--- src/app/components/nav/Nav.tsx
+++ src/app/components/nav/Nav.tsx
@@ -3,22 +3,22 @@ import React from 'react'
 
 const Nav = () => {
   return (
-    <nav className='flex justify-between py-6 px-16'>
+    <nav className='flex justify-between py-8 px-16'>
         <div className=''></div>
         <div className='flex justify-center'>
             <Link href={"/"} className='px-4'>
                 Home
             </Link>
-            <Link href={"/requests"} className='px-4'>
+            <Link href={"/requests"} className='px-5'>
                 Request
             </Link>
-            <Link href={"/donate"} className='px-4'>
+            <Link href={"/donate"} className='px-5'>
                 Donate
             </Link>
-            <Link href={"/history"} className='px-4'>
+            <Link href={"/history"} className='px-5'>
                 History
             </Link>
-            <Link href={"/profile"} className='px-4'>
+            <Link href={"/profile"} className='px-5'>
                 Profile
             </Link>
         </div>
