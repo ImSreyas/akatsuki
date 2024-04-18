711856e0627f6ceb5019dc6c4d5623d80b8d2864
ImSreyas <sreyassatheesh755@gmail.com>
2024-04-18 09:40:14 +0530

    Font changed.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,13 @@ const Home = async () => {
 
   return (
     <div>
-      
+      <div className='first-section'>
+        <h1 className='text-[50px]'>Zero Hunger</h1>
+        <div className='desc'>
+          Lets make the dream of Zero Hunger possible by making donations to people needy or starving food which would be a way for us to show our caring for fellow beings and helping humanity to be hunger free. We need your help for making this dream a reality. Please make a donation and become a part of this vision.
+        </div>
+      </div>
+      <div className='second-section'></div>
     </div>
   )
 }
