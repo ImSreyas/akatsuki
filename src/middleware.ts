14776d7fc16c6ecf8b9a823f3d0b613a8d5e06a5
ImSreyas <sreyassatheesh755@gmail.com>
2024-04-19 13:53:32 +0530

    Blur effect in home.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware({});
+export default authMiddleware({publicRoutes: ["/history"]});
 
 export const config = {
   matcher: ['/((?!api|home|_next/static|_next/image|favicon.ico|$).*)'],
